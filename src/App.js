ae61822af78ef2a5c1daacc676130eeb839f2c5c
Richard He <richardhe330@gmail.com>
2023-05-26 10:48:22 -0600

    refining

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import {Route, Routes} from 'react-router-dom'
 import Login from './pages/Login'
 import Dashboard from "./pages/Dashboard";
-import Profile from './pages/Profile'
 import ProtectedRoutes from './components/ProtectedRoutes';
 
 function App() { 
@@ -9,12 +8,12 @@ function App() {
     <>      
     <Routes>
         <Route path="/" element={<Login />} />
-        <Route element={<ProtectedRoutes />} />
+        <Route element={<ProtectedRoutes />} >
           <Route path="dashboard" element={<Dashboard />} />
+        </Route>
      </Routes> 
        
     
-      
     </>
   );
 }
