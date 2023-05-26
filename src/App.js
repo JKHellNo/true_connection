60c2cd15eba49657cf9154f5968e0b74f00f853b
Richard He <richardhe330@gmail.com>
2023-05-26 11:03:01 -0600

    top of page resolved

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import Registration from './components/Login'
+import { Routes, Route } from 'react-router-dom';
+import Registration from './pages/Login';
 import Header from './components/Header'
 import Navigation from './components/Navigation'
+import ProtectedRoutes from './components/ProtectedRoutes';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 
 
 function App() { 
   return ( 
-    <div className="app">      
-      <Header /> 
-      <Registration /> 
-    
+    <div className="app">     
+      
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route element={<ProtectedRoutes />} >
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+     </Routes> 
       
     </div>
   );
