65c774550e564fca23d807da8b174d011763cab1
JKHellNo <richardhe330@gmail.com>
2023-05-31 20:44:47 -0600

    postform import deletion error

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter} from "react-router-dom";
-import PostForm from './components/PostForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
